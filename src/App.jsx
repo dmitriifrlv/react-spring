b039b569c09040e8e485b5bd2dd4a21c0330f641
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2021-09-03 18:17:18 +0500

    props interpolation

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useSpring, animated, config } from "react-spring";
+import { useState } from "react";
+
+function App() {
+  const [toggle, setToggle] = useState(false);
+  const { background, z } = useSpring({
+    background: toggle ? "red" : "green",
+    y: !toggle ? 0 : 50,
+    z: !toggle ? 0 : 150,
+  });
+
+  const props = useSpring({
+    from: {
+      borderRadius: "0",
+    },
+    to: {
+      borderRadius: " 50%",
+    },
+    config: config.gentle,
+  });
+
+  return (
+    <div className="App">
+      <button onClick={() => setToggle(!toggle)}>Test</button>
+      <div className="container">
+        <animated.div
+          className="rectangle"
+          style={{
+            background,
+            transform: z.to((z) => `translate3d(${z}px, ${z}px, 0)`),
+          }}
+        >
+          I will fade in
+        </animated.div>
+
+        <animated.div className="rectangle" style={props}></animated.div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
